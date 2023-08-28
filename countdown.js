58f5a72104f4fbe341cf56b937da27aa7dfdbbc1
iammahipatsinh <143388961+iammahipatsinh@users.noreply.github.com>
2023-08-28 13:41:04 +0530

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,20 @@
+// Set the launch date for the countdown
+const launchDate = new Date("2024-01-01T00:00:00").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeRemaining = launchDate - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
